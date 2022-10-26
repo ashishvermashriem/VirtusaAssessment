7694c6a01353b5beea88b19b2eafed279023ab96
ashishvermashriem <106428430+ashishvermashriem@users.noreply.github.com>
2022-10-26 22:19:43 +0530

    Initial commit

---
+++ VirtusaAssessment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VirtusaAssessment
+//
+//  Created by Preeti Dhankar on 26/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
